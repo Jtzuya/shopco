7f433d845e6f05737d84691ac6144b2a5073257a
jtzuya <jtzuya@gmail.com>
2024-07-27 10:12:12 +0800

    add: id type & link to edit product

--- src/components/admin/Table.tsx
+++ src/components/admin/Table.tsx
@@ -1,8 +1,10 @@
 // import 'dotenv/config'
 // const serverOrigin = process.env.REACT_APP_SERVER
+import { Link } from 'react-router-dom';
 import { useEffect, useRef, useState, MutableRefObject } from 'react'
 
 interface Product {
+  id: number;
   name: string;
   stock: number;
 }
@@ -65,7 +67,9 @@ export default function Table(props: TableProps) {
                 <div className="table__cell">
                   <input type="checkbox" name="select_product" id="select_product" ref={itemRefs[idx]}/> 
                 </div>
-                <div className="table__cell">{product.name}</div>
+                <div className="table__cell">
+                  <Link to={`/admin/product/${product.id}`}>{product.name}</Link>
+                </div>
                 <div className="table__cell">Active</div>
                 <div className="table__cell">
                   {/* <p>30 in stock for 3 variants 30 in stock for 3 variants</p> */}
