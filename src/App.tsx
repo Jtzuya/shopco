7d374a663249ab073434a7da8258a10716e9bc37
jtzuya <jtzuya@gmail.com>
2024-07-27 10:10:41 +0800

    update: remove origin & add clear cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useActionData } from 'react-router-dom'
 // import { useState } from 'react'
 
 /** pages */
@@ -17,9 +17,17 @@ import AdminTransaction from './pages/admin/Transaction'
 
 /** styles */
 import './styles/App.scss'
+import { useEffect } from 'react'
 
 function App() {
-  const origin = window.origin;
+  useEffect(() => {
+    const clearCache = setTimeout(() => {
+      localStorage.clear();
+    }, 1000 * 60 * 5)
+
+    // Cleanup function to clear the timeout if the component unmounts
+    return () => clearTimeout(clearCache)
+  }, [])
 
   return (
     <BrowserRouter>
@@ -29,21 +37,21 @@ function App() {
         which page it will render.
       */}
       <Routes>
-        <Route path={`${origin}/`} element={<Home />} />
-        <Route path={`${origin}/on-sale`} element={<OnSale />} />
-        <Route path={`${origin}/new-arrivals`} element={<NewArrivals />} />
-        <Route path={`${origin}/brands`} element={<Brands />} />
-        <Route path={`${origin}/cart`} element={<Cart />} />
-        <Route path={`${origin}/user`} element={<User />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/on-sale' element={<OnSale />} />
+        <Route path='/new-arrivals' element={<NewArrivals />} />
+        <Route path='/brands' element={<Brands />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/user' element={<User />} />
 
         {/* Protected Routes */}
-        <Route path={`${origin}/admin`} element={<Admin />} /> {/** Overview */}
-        <Route path={`${origin}/admin/product-list`} element={<AdminProductList />} />
-        <Route path={`${origin}/admin/product`} element={<AdminProduct />} />
-        <Route path={`${origin}/admin/product/new`} element={<AdminProduct />} />
-        <Route path={`${origin}/admin/product/:id`} element={<AdminProduct />} />
-        <Route path={`${origin}/admin/transaction-list`} element={<AdminTransactionList />} />
-        <Route path={`${origin}/admin/transaction`} element={<AdminTransaction />} />
+        <Route path='/admin' element={<Admin />} /> {/** Overview */}
+        <Route path='/admin/product-list' element={<AdminProductList />} />
+        <Route path='/admin/product' element={<AdminProduct />} />
+        <Route path='/admin/product/new' element={<AdminProduct />} />
+        <Route path='/admin/product/:id' element={<AdminProduct />} />
+        <Route path='/admin/transaction-list' element={<AdminTransactionList />} />
+        <Route path='/admin/transaction' element={<AdminTransaction />} />
       </Routes>
     </BrowserRouter>
   )
